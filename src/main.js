50493a902e2810b5edbbe2589ef65da6bb6fc305
MizarZh <2267243023@qq.com>
2020-04-24 00:25:44 +0800

    tested moving function

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-(function($){
+/* (function($){
     // 弹幕发射实现
     $('#danmu').load(function(){
 
     })
 
     // 弹幕默认设置
-/*     defaultSetting = {
+    defaultSetting = {
         fontSize  : 16,
         color     : 'white',
         position  : 'top',
@@ -17,10 +17,10 @@
 
     function Danmu2(text = '', start = 0, end = 1000, color = '#fff', row = 1, fontSize = 16){
 
-    } */
+    }
 
     // 弹幕类
-/*     function Danmu(text, setting){
+    function Danmu(text, setting){
         if(this instanceof Danmu){
             if(setting === void 0){
                 this.setting = defaultSetting;
@@ -32,7 +32,7 @@
             return new Danmu(text, setting);
         }
         return this;
-    } */
+    }
 
 
     // 填充机制应为：如果弹幕仍在入口处，则将其移到第二行，如果都满了才重叠 
@@ -47,9 +47,12 @@
 
     }
 
-})(jQuery);
+})(jQuery); */
 
 
+
+var div = document.querySelector('#danmu');
+var start = Date.now();
 
 function json2Danmu(){
 
@@ -65,19 +68,50 @@ function Danmu(text, start, duration = 3, color = '#fff', fontSize = 16){
     this.fontSize = fontSize;
 }
 
-// 可以试试直接使用setTimeout运行并销毁弹幕
 // 判断是否在下一行可以设置一个delay的时间，当小于这个时间时则下一行
 
 function renderByTime(sortedDanmuArray, rows){
-    sortedDanmuArray
+    var DanmuItem = sortedDanmuArray[0];
+    var delay;
+    var timeline;
+    var timelineId = setInterval(function(){
+        timeline = Date.now() - start;
+    },1)
     var run = function(DanmuItem){
         var DanmuContainer = document.createElement('div');
-        DanmuContainer.classList.add('.rowitems');
-        DanmuContainer.style = '';
-
+        DanmuContainer.innerText = DanmuItem.text; // 赋值
+        DanmuContainer.classList.add('rowitems');  // 使用rowitems规范化
+        DanmuContainer.style = '';  //这里加其他属性
+        DanmuContainer.style.left = div.clientLeft + div.clientWidth + 'px';
+        var speed = (div.clientLeft + div.clientWidth - div.clientLeft - DanmuContainer.clientWidth)
+            / DamuItem.duration;
+        div.querySelectorAll('.rows')[rows-1].appendChild(DanmuContainer);
+        var runId = setInterval(function(){
+            left = parseFloat(DanmuContainer.style.left)
+            if(left < -DanmuContainer.clientWidth) {clearInterval(runId); } // 还需要加一个移除元素
+            DanmuContainer.style.left = left - speed + 'px';
+        },2)
     }
 }
 
 function renderByRealtime(){
 
 }
+
+// 测试用
+
+var div = document.querySelector('#danmu');
+var start = Date.now();
+var duration = 1000;
+DanmuContainer = document.querySelector('.rowitems');
+DanmuContainer.style.left = div.clientLeft + div.clientWidth + 'px';
+var speed = (div.clientWidth + DanmuContainer.clientWidth) / duration;
+var runId = setInterval(function(){
+    left = parseFloat(DanmuContainer.style.left)
+    if(left < -DanmuContainer.clientWidth) {
+        clearInterval(runId);
+        document.querySelectorAll('.rows')[0].removeChild(DanmuContainer);
+    }
+    DanmuContainer.style.left = left - speed + 'px';
+    
+},1)
