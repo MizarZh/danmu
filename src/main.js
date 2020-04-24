6ef026d7726e7d3e27f5dfe72afbebab32e82a23
MizarZh <2267243023@qq.com>
2020-04-24 15:52:27 +0800

    finish renderSingleDanmu and timeline

--- src/main.js
+++ src/main.js
@@ -52,15 +52,15 @@
 
 
 var div = document.querySelector('#danmu');
-var start = Date.now();
-
+var tracks = document.querySelectorAll('.rows'); // 后期可能需要用js生成
+var maxRow = 4; // 最大行数为4
 function json2Danmu(){
 
 }
 
 function Danmu(text, start, duration = 3, color = '#fff', fontSize = 16){
     if(text === undefined && start === undefined && duration === undefined) 
-    throw new Error('first three arguments are nessisary');
+    {throw new Error('first three arguments are nessisary');}
     this.text = text;
     this.start = start;
     this.duration = duration;
@@ -68,30 +68,67 @@ function Danmu(text, start, duration = 3, color = '#fff', fontSize = 16){
     this.fontSize = fontSize;
 }
 
-// 判断是否在下一行可以设置一个delay的时间，当小于这个时间时则下一行
+
 
 function renderByTime(sortedDanmuArray, rows){
-    var DanmuItem = sortedDanmuArray[0];
-    var delay;
-    var timeline;
-    var timelineId = setInterval(function(){
-        timeline = Date.now() - start;
-    },1)
-    var run = function(DanmuItem){
+    // sortedDanmuArray为按照时间线排序好的Danmu类数组
+    var timeline = 0,   // 时间线
+    currentRow = 1, // 目前弹幕所在行数
+    index = 0;      // sortedDanmuArray运行到的位置
+    timelineStart = Date.now()
+
+    var init = function(newtimeline){
+        // 清楚所有弹幕元素并初始化参数
+        for(let rows of document.querySelectorAll('.rows')) rows.innerHTML = '';
+        timeline = newtimeline,currentRow = 1,index = 0,timelineStart = Date.now();
+
+        // 二分查找起始index
+
+
+        // 
+    }
+
+    var renderSingleDanmu = function(DanmuItem, row){
+        // 属性设置
         var DanmuContainer = document.createElement('div');
-        DanmuContainer.innerText = DanmuItem.text; // 赋值
+        DanmuContainer.innerText = DanmuItem.text;
         DanmuContainer.classList.add('rowitems');  // 使用rowitems规范化
-        DanmuContainer.style = '';  //这里加其他属性
+        DanmuContainer.style = 
+        `color : ${DanmuItem.color}; font-size : ${DanmuItem.fontSize};`;  //其他属性
+
+        // 确定位置
         DanmuContainer.style.left = div.clientLeft + div.clientWidth + 'px';
+        //速度值仍不准
         var speed = (div.clientLeft + div.clientWidth - div.clientLeft - DanmuContainer.clientWidth)
             / DamuItem.duration;
-        div.querySelectorAll('.rows')[rows-1].appendChild(DanmuContainer);
+        row.appendChild(DanmuContainer);
+
+        // 运动
         var runId = setInterval(function(){
             left = parseFloat(DanmuContainer.style.left)
-            if(left < -DanmuContainer.clientWidth) {clearInterval(runId); } // 还需要加一个移除元素
+            if(left < -DanmuContainer.clientWidth) {
+                clearInterval(runId); 
+                div.querySelectorAll('.rows')[row-1].remove(DanmuContainer)
+            }
             DanmuContainer.style.left = left - speed + 'px';
-        },2)
+        },1)
     }
+
+    // 更新时间线并放置弹幕
+    var timelineId = setInterval(function(){
+        timeline = Date.now() - timelineStart; 
+        var danmuPerRow;
+        while(timeline > sortedDanmuArray[index]){
+            for(let i of tracks){
+                danmuPerRow[danmuPerRow.length] = i.childElementCount; // 每行弹幕的个数
+            }
+            renderSingleDanmu(sortedDanmuArray[index], 
+                tracks[ [...tracks].indexOf(Math.min(danmuPerRow))] ); // 返回元素最少的
+            ++index;
+        }
+        if(index > sortedDanmuArray.length) clearInterval(timelineId);
+    },1)
+
 }
 
 function renderByRealtime(){
@@ -101,16 +138,16 @@ function renderByRealtime(){
 // 测试用
 
 var div = document.querySelector('#danmu');
-var start = Date.now();
-var duration = 1000;
-DanmuContainer = document.querySelector('.rowitems');
+var timelineStart = Date.now();
+var duration = 7000; // 不准
+DanmuContainer = document.querySelectorAll('.rowitems')[0];
 DanmuContainer.style.left = div.clientLeft + div.clientWidth + 'px';
-var speed = (div.clientWidth + DanmuContainer.clientWidth) / duration;
+var speed = (div.clientWidth + DanmuContainer.clientWidth) / (duration / 10);
 var runId = setInterval(function(){
     left = parseFloat(DanmuContainer.style.left)
     if(left < -DanmuContainer.clientWidth) {
-        clearInterval(runId);
         document.querySelectorAll('.rows')[0].removeChild(DanmuContainer);
+        clearInterval(runId);
     }
     DanmuContainer.style.left = left - speed + 'px';
     
